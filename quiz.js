77a71f6c7146aaa46ce27aff72a1925188a186dc
Vikkee Patel <95123524+anivicky@users.noreply.github.com>
2023-06-05 22:25:51 +0530

    Add files via upload
    quiz website for indian history.

---
+++ quiz.js
@@ -0,0 +1,107 @@
+let quiz_rules = [
+    "You have to solve given Question within a time limit given to you.",
+    "Once you select any anwer. It can't be undone",
+    "You'll get marks on the basic of your correct answers.",
+    
+];
+
+
+let quiz_questions = [
+    // question 
+    {
+        question: "Who was the first President of India?",
+
+        options : [
+            " Dr. Rajendra Prasad",
+            " Mahatma Gandhi",
+            " Dr. B. R. Ambedkar",
+            " Pandit Jawaharlal Nehru",
+        ],
+
+        // correct answer
+        answer: 1,
+    },
+    
+    {
+        question: "Which is the most sensitive organ in our body ?",
+    
+        options: [
+            "Tongue",
+            "Skin",
+            "Nose",
+            "Eyes",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Who invented Computer?",
+    
+        options: [
+            "Charles Babbage",
+            "James Gosling",
+            "Guido van Rossum",
+            "John McCarthy",
+        ],
+        answer: 1,
+
+    },
+    {
+        question: "1024 Kilobytes is equal to?",
+    
+        options: [
+            "1 Gigabyte (MB)",
+            "10 Megabyte (MB)",
+            "1 Megabyte (MB)",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Most widely spoken language in the world is?",
+    
+        options: [
+            "English",
+            "Mandarin (Chinese)",
+            "Hindi",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Who is the founder of Microsoft?",
+    
+        options: [
+            "Bill Gates",
+            "Sergey Brin",
+            "Steve Jobs",
+            
+        ],
+
+        answer: 1,
+    },
+    {
+
+        question: "Does UPI stands for Unified Payments Interface ?",
+    
+        options: [
+            "False",
+            "True",
+        ],
+        
+        answer: 2,
+
+    },
+    {
+        
+        question: "Which of the following is the correct extension of the Python file?",
+    
+        options: [
+            '.python',
+            '.pl',
+            '.py',
+            '.p',
+        ],
+        
+        answer: 3,
+    
+    },
+    
+];
